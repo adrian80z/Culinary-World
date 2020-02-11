ff4943a747cb8809e12fd665657ef366fbd8f17e
Adrian <adrian.ziolkowski@gmail.com>
2020-02-11 16:11:52 +0000

    route for recipes.html added

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 from os import path
+
 if path.exists("env.py"):
-    import env 
+    import env
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+
+@app.route("/")
+def all_recipes():
+    return render_template("recipes.html", title="Home",
+    cuisine_type = mongo.db.cousine_type.find())
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
+
