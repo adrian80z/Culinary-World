ad92971f846b71abb7d964b99db8e4f931f86d5a
Adrian <adrian.ziolkowski@gmail.com>
2020-02-12 17:18:27 +0000

    route and view for delete_recipe added

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def edit_recipe(recipe_id):
 
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
-    mongo.db.recipes.remove({"id": ObjectId(recipe_id)})
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     return redirect(url_for("all_recipes"))
 
 
