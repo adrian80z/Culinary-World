d2bab0346f346548876335906b2a7fc6e76c31b7
Adrian <adrian.ziolkowski@gmail.com>
2020-02-26 15:34:49 +0000

    js code moved from base.html

---
+++ static/js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.slider').slider();
+    $('select').formSelect();
+    $('.modal').modal();
+    $(".dropdown-trigger").dropdown();
+
+    /* Dynamically add new input field to ingredients form*/
+    $(".add-input-field").on("click", function () {
+        $('<input type="text" class="validate" name="ingredients" placeholder="Add next ingredient" required >').insertBefore(".add-input-field");
+    });
+
+    /* Removes last input element from ingredients form*/
+    $(".delete-last-field").on("click", function () {
+        $("#items input:last").remove();
+    });
+
+    /* Dynamically add new input field to steps form */
+    $(".add-step").on("click", function () {
+        $('<textarea name="method" class="materialize-textarea" placeholder="Add next step" required ></textarea>').insertBefore(".add-step");
+    });
+ 
+    /* Removes last input element from steps form */
+    $(".delete-last-step").on("click", function () {
+        $("#steps textarea:last").remove();
+    });
+});
+
+
+/* page printing function */
+function printRecipe() {
+    window.print();
+}
